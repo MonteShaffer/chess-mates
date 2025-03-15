46bdfa750ee8059cad36c0375d4de3ab6899aa6e
MonteShaffer <monte.shaffer@gmail.com>
2025-03-15 11:41:18 -0600

    proc blocking issues in Windoze

--- web/class.stockfish.php
+++ web/class.stockfish.php
@@ -6,44 +6,81 @@ class stockfish
 	private $pipes;
 	private $offset;
 	public $out;
-	public $parsed;
 	
-	public function __construct($EXE = "C:\stockfish\stockfish.exe")
-		{
+	public function __construct($EXE = "C:/stockfish/stockfish.exe")
+		{		
+		$this->EXE		= $EXE;
 		$descr = array	(
 						0 => array("pipe", "r"), 	#iStream as stdin
 						1 => array("pipe", "w"),	#oStream as stdout
 						2 => array("pipe", "w")		#eStream as stderr
 						);
-		$this->pipes = array();
-		$this->offset = array(0,0,0);
-		$this->out = array	(
-								"commands" 	= array(), 
-								"stream"	= "", 
-								"payload"	= array()
+		$this-> STDOUT = dirname(__FILE__). "/". "stdout.txt";
+		
+		$descr = array	(
+						0 => array("pipe", "r"), 	#iStream as stdin
+						1 => array("file", $this-> STDOUT, "w"),	#oStream as stdout
+						2 => array("pipe", "w")		#eStream as stderr
+						);
+						
+						
+						
+		$this->pipes 	= array();
+		$this->offset 	= array(0,0,0);
+		$this->out 		= array(
+								"commands" 	=>	array(),
+								"stream"	=>	array(),
+								"payload"	=>	array()
 								);
-		$this->process = proc_open($EXE, $descr, $this->pipes);
+		$this->stream 	= "";
+		$this->process 	= proc_open($this->EXE, $descr, $this->pipes);
+		
+		# https://www.php.net/manual/en/function.stream-get-contents.php
+		# stream_set_blocking($this->pipes[1], 0);
+		
+		
+		$this->timeout 	= 5; # isComplete timeout (in seconds)
+		$this->start 	= microtime(true);
+		$this->now		= microtime(true);
 		}
 	
 	public function isComplete($what="bestmove",$micro=10000)
 		{
+		$this->now = microtime(true); 
+		$this->time = $this->now - $this->start;
+		if($this->time > $this->timeout) { return false; }
 		# isready returns "ok" even when search is still ongoing
-		while (strpos($this->out, $what) === false) 
+		while (strpos($this->stream, $what) === false) 
 			{
 			# set a delay before checking again
 			usleep($micro);
 			$this->getStream();
 			}
+		return true;
 		}
 	public function getStream()
 		{
-		$this->out["stream"][microtime(true)] .= stream_get_contents	
+			/*
+		$contents = "";
+		while ( fgets($this->pipes[1]) !== false ) 
+			{
+			$contents .= fgets($this->pipes[1])."\n";
+			}	
+		print_r($contents); exit;
+		*/
+		/*
+		$this->out["stream"][microtime(true).""] = stream_get_contents	
 												(	$this->pipes[1],
 													-1,
 													$this->offset[1]
-												) . "\n";
+												);
 		# update latest marker from stdout 
-		$this->offset[1] = ftell(this->pipes[1]);		
+		$this->offset[1] = ftell(this->pipes[1]);	
+		*/
+		#$this->stream = stream_get_contents($this->pipes[1]);
+		$this->stream = file_get_contents($this-> STDOUT);
+		print_r($this);  #exit;
+		
 		}
 		
 	public function setOption($option="UCI_LimitStrength",$value="true")
@@ -55,13 +92,17 @@ class stockfish
 		}
 	public function sendCommand($cmd="uci")
 		{
+		$this->now = microtime(true);
 		# if (is_resource($this->process)) {
-		$this->out["commands"][microtime(true)] = $cmd; # log activity
+		$this->out["commands"][$this->now.""] = $cmd; # log activity
 		
 		fwrite($this->pipes[0], $cmd."\n");
 		fflush($this->pipes[0]);
-		$this->getStream();
 		
+		# file_put_contents($this -> STDOUT,"\n\n##### cmd[".$this->now."]: ".$cmd." #####\n\n", FILE_APPEND);
+		
+		
+		$this->getStream();
 		# https://official-stockfish.github.io/docs/stockfish-wiki/UCI-&-Commands.html
 		# position startpos
 		# position startpos moves e2e4 e7e5 g1f3
@@ -82,7 +123,8 @@ class stockfish
 		# go movetime 5000
 		}
 		
-	public evalPosition = function($fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+	#public evalPosition = function($fen ="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+	public function evalPosition ($fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 		{
 		$this->sendCommand("uci");
 		$this->sendCommand("position fen ".$fen);
@@ -91,7 +133,7 @@ class stockfish
 		
 		
 		}
-	public findBestMove = function($fen,$method="depth 10")
+	public function findBestMove ($fen, $method = "depth 10")
 		{
 		$this->sendCommand("uci");
 		$this->sendCommand("position fen ".$fen);
@@ -100,20 +142,20 @@ class stockfish
 		
 		} 
 	
-	public parseBestMove = function($str)
+	public function parseBestMove($str)
 		{		
 		$data = array();
 		
 		return $data;
 		}
-	public parseEval = function($str)
+	public function parseEval($str)
 		{
 		$data = array();
 		
 		return $data;
 		}
 
-	public close = function()
+	public function close()
 		{
 		fclose($this->pipes[0]);
 		fclose($this->pipes[1]);
--- web/stockfish.php
+++ web/stockfish.php
@@ -1,69 +1,17 @@
 <?PHP
 echo"<PRE>";
-# https://stackoverflow.com/questions/31817146/running-stockfish-chess-engine-under-php-script
-// ok, define the pipes
-$descr = array(
+include("class.stockfish.php");
+$stockfish = new stockfish();
 
-    0 => array("pipe", "r"),
-    1 => array("pipe", "w"),
-    2 => array("pipe", "w")
-);
+$stockfish->evalPosition();
+# sleep(5);
+# $stockfish->now = microtime(true);
 
-$pipes = array();
 
-// open the process with those pipes
-$process = proc_open("C:\stockfish\stockfish.exe", $descr, $pipes);
+$stockfish->close();
 
-// check if it's running
-if (is_resource($process)) {
+print_r($stockfish);
 
-    // send first universal chess interface command
-    fwrite($pipes[0], "uci\n");
-	// send position
-    #fwrite($pipes[0], "position startpos\n");
-	fwrite($pipes[0], "position startpos moves e2e4 e7e5 g1f3\n");
-	// isready?
-    fwrite($pipes[0], "isready\n");
-	fwrite($pipes[0], "d\n");
-	// send analysis (depth 10) command
-    fwrite($pipes[0], "go depth 15\n");
-	fwrite($pipes[0], "isready\n");
-    #// send analysis (5 seconds) command
-    #fwrite($pipes[0], "go movetime 5000");
-sleep(2);  # make this a function of the contents containing "bestmove"???
-    // close read pipe or STDOUTPUT can't be read
-    fclose($pipes[0]);
-	
-function getContentsFromPipe($pipe)
-	{
-	$str = "";
-    // read and print all output comes from the pipe
-    while (!feof($pipe)) 
-		{
-		$str .= fgets($pipe)."\n";
-		}
-	return $str;
-	}
-
-$out = getContentsFromPipe($pipes[1]);
-print_r($out);
-
-echo"MONTE\n\n";
-$out = "";
-    // read and print all output comes from the pipe
-    while (!feof($pipes[1])) {
-		$out .= fgets($pipes[1])."\n";
-        #echo fgets($pipes[1]);
-
-    }
-
-    // close the last opened pipe
-    fclose($pipes[1]);
-
-    // at the end, close the process
-    proc_close($process);
-
-echo"<PRE>";print_r($out);exit;
-}
+echo"</PRE>";exit;
 
 ?>
